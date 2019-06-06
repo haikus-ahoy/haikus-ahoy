862d426de8cbaf3443be992ec38510402dff8e18
terrirp <terri.persad@gmail.com>
2019-06-06 19:16:59 -0400

    created if/else conditional to check syllable to put in appropriate haiku line

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ class App extends Component {
     super();
 
     this.state = {
-      lineOne: [],
+      lineOne:[],
+      lineTwo:[],
+      lineThree:[],
+      wholeHaiku: [],
       getUserWord: [],
+      userWord: '',
       seedWord: '',
       allBoatWords:[],
       wordOptions: [],
       currentMatches: [],
       currentThreeBoatWords: [],
-      lineOneSyllables: 0,
       lineTwoSyllables: 0,
       lineThreeSyllables: 0,
     }
@@ -43,7 +46,7 @@ class App extends Component {
       response = response.data;
       // a variable to store the word value from the first entry in the array
       const word = response[0].word;
-      console.log('this is word', word, response)
+      
       // a variable to store the number of syllables value from the first entry in the array
       const numSyllables = response[0].numSyllables;
       // console.log(`"${word}" has ${numSyllables} syllables.`);
@@ -51,19 +54,19 @@ class App extends Component {
       if (seedWord === word && numSyllables < 6) {
         // console.log(`The first line has ${5 - numSyllables} syllables remaining.`);
         // a variable that holds a copy of the line one array in state
-        const newFirstLine = [...this.state.lineOne];
+        const newFirstLine = [...this.state.wholeHaiku];
         // pushes the word and number of syllables values of the the user input word to the newFirstLine array
         newFirstLine.push({ 'word': word, 'numSyllables': numSyllables})
         this.getWordSuggestions(); 
         //counting the first word
         const firstWordSyllableCount = this.countSyllables(newFirstLine);
-        console.log('first word syllable count', firstWordSyllableCount)
-        // sets state of lineOne to be equal to the value of newFirstLine
+       
+        // sets state of wholeHaiku to be equal to the value of newFirstLine
         this.setState({
-          lineOne: newFirstLine,
-          lineOneSyllables: firstWordSyllableCount,
+          wholeHaiku: newFirstLine,
+          seedWord: ''
         });
-        // console.log(this.state.lineOne);
+        // console.log(this.state.wholeHaiku);
         // if the user's word has too many syllables, prompt an error
 
         //set state for 
@@ -95,9 +98,13 @@ class App extends Component {
     })
     // a promise to return a successful or unsuccessful API call
     .then((response) => {
+      
+      const filterPunctuation = response.data.filter((hit) => {
+        return hit.word !== ".";
+      })
       const wordOptionsArray = [];
       for(let i = 0; i < 7; i++){
-        wordOptionsArray.push(response.data[i])
+        wordOptionsArray.push(filterPunctuation[i])
       }
       // filter undefined hits
       const filteredWordOptions = wordOptionsArray.filter(element => {
@@ -106,9 +113,9 @@ class App extends Component {
       const threeBoatWords = this.grabThree();
       ////////////////////////////////////////////////////////////
       //we need to concat the chosenboat words that we will creat
-      console.log(this.state.currentThreeBoatWords, 'these boat words now')
+    
       const fullWordOptionsArray = filteredWordOptions.concat(this.state.currentThreeBoatWords);
-      console.log('FULL OPTIONS ARRAY: ', fullWordOptionsArray);
+
       this.setState({
         wordOptions: fullWordOptionsArray,
       })
@@ -188,42 +195,62 @@ class App extends Component {
     //event prevent default
     event.preventDefault();
     //saving line one in a copy
-    const lineOne = [...this.state.lineOne]
+    const wholeHaiku = [...this.state.wholeHaiku]
     //creating a variable for the new word
     const newWord = this.state.wordOptions[index]
-    console.log(this.state.wordOptions, "this is the new word")
-    //push the new word to the lineOne array
-    lineOne.push(newWord)
-    //set state so that lineOne is the new lineOne and seedWord is the newWord's property of word
+   
+    //push the new word to the wholeHaiku array
+    wholeHaiku.push(newWord)
+    //set state so that wholeHaiku is the new wholeHaiku and seedWord is the newWord's property of word
     this.setState ({
-      lineOne: lineOne,
+      wholeHaiku: wholeHaiku,
       seedWord: newWord.word,
+      // seedWord: '',
       }, 
       //doing a callback to the getWordSuggestions so we can repopulate the options
       () => {
         //call get word suggestions to repopulate the next word options 
         this.getWordSuggestions()
-        const currentSyllables = this.countSyllables(lineOne)
-        this.setState({
-          lineOneSyllables: currentSyllables
-        }, () => {
-          console.log(this.state.lineOneSyllables, "this is line one syllables")
-        })
+        const currentSyllables = this.countSyllables(wholeHaiku)
+        this.distributeSyllables();
       }
     )
   }
 
-  //sends the number of syllables to the lineOneSyllables array
-  countSyllables = (lineOne) => {
-    const syllableReduce = lineOne.reduce((total, word) =>{
+  //sends the number of syllables to the wholeHaikuSyllables array
+  countSyllables = (haikuArray) => {
+    const syllableReduce = haikuArray.reduce((total, word) =>{
       return total + word.numSyllables
     }, 0 )
     return syllableReduce
   }
-
-
   
+  distributeSyllables = () => {
+     const wholeHaiku = [...this.state.wholeHaiku];
+     const lineOne =[];
+     const lineTwo =[];
+     const lineThree= [];
+      wholeHaiku.forEach((count, i) => {
+        const haikuSlice = wholeHaiku.slice(0, i+1);
+        const sliceSyllables = this.countSyllables(haikuSlice);
+       
 
+        if (sliceSyllables < 6){
+          lineOne.push(wholeHaiku[i]);
+        } else if (sliceSyllables < 13){
+          lineTwo.push(wholeHaiku[i])
+        }else if (sliceSyllables < 18){
+          lineThree.push(wholeHaiku[i])
+        }
+          
+  })
+   this.setState({
+     lineOne: lineOne,
+     lineTwo: lineTwo,
+     lineThree: lineThree,
+   })
+}
+  
 
   render() {
     return (
@@ -231,7 +258,7 @@ class App extends Component {
         <form action="submit">
           <label htmlFor="word">Input Starting Word Here</label>
           <input onChange={this.handleChange} value={this.state.seedWord} id="word" name="word" type="text" />
-          <button onClick={this.handleClick}>Submit</button>
+          <button disabled={this.state.wholeHaiku.length > 0 ? true : false } onClick={this.handleClick}>Submit</button>
         </form>
 
         <div className="nextWordOption">
@@ -241,7 +268,7 @@ class App extends Component {
             {this.state.wordOptions.map((result, i) => {
               return (
                 // Creating an onClick listener for each button appended to the page 
-                <li key={i}><button onClick={(event) => { this.buttonWordChoice(event, i)} } className="wordButton">
+                <li key={i}><button  onClick={(event) => { this.buttonWordChoice(event, i)} } className="wordButton">
                 {result.word}
                 </button></li>
               )
@@ -249,14 +276,42 @@ class App extends Component {
           </ul>
 
         </div>
-        <div>
-          <h2>Haiku</h2>
+        <h2>Haiku</h2>
+        <div className="Flex-Parent">
+         
           <div className="lines">
             <h3>Line One</h3>
             <ul>
               {this.state.lineOne.map((result, i) => {
                 return (<li key={i}>{result.word}</li>)
                 }
+              )}
+            </ul>
+          </div>
+          <div>
+            <h3>Line Two</h3>
+            <ul>
+              {this.state.lineTwo.map((result, i) => {
+                return (<li key={i}>{result.word}</li>)
+              }
+              )}
+            </ul>
+          </div>
+          <div>
+            <h3>Line Three</h3>
+            <ul>
+              {this.state.lineThree.map((result, i) => {
+                return (<li key={i}>{result.word}</li>)
+              }
+              )}
+            </ul>
+          </div>
+          <div>
+            <h3>Whole Haiku</h3>
+            <ul>
+              {this.state.wholeHaiku.map((result, i) => {
+                return (<li key={i}>{result.word}</li>)
+              }
               )}
             </ul>
           </div>
