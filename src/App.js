df1a40301644a108efc2093096cb2e148c2ee639
Darwin Lyons <whoischarles@gmail.com>
2019-06-10 19:40:10 -0400

    fix scroll chrome

--- src/App.js
+++ src/App.js
@@ -429,7 +429,7 @@ class App extends Component {
                   <form  action="submit">
                     <label htmlFor="word" className="visuallyHidden">Input Starting Word Here</label>
                     <input className="Input" onChange={this.handleChange} placeholder="Enter your Starting Word" value={this.state.seedWord} id="word" name="word" type="text" disabled={this.state.wholeHaiku.length > 0 ? true : false}/>
-                    <button disabled={this.state.wholeHaiku.length > 0 ? true : false } onClick={this.handleClick} className="Submit">Submit</button>
+                    <button disabled={this.state.wholeHaiku.length > 0 ? true : false } onClick={this.handleClick} className="Submit"><a href="#ContainerHaiku">Submit</a></button>
                   </form>
 
               {/* form */}
