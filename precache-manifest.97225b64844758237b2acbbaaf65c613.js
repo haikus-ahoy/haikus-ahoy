917b535be70375e815c4550bec49124b66c297f6
julienbigras <julien.bigras@hotmail.com>
2019-06-11 16:09:55 -0400

    Updates

---
+++ precache-manifest.97225b64844758237b2acbbaaf65c613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ad64658058cd1f952940a12eb2dc8f",
+    "url": "/haikus-ahoy/index.html"
+  },
+  {
+    "revision": "d31e4cf38885cec37118",
+    "url": "/haikus-ahoy/static/css/main.47b3c634.chunk.css"
+  },
+  {
+    "revision": "eb4f52460fc14b2d81fd",
+    "url": "/haikus-ahoy/static/js/2.27995689.chunk.js"
+  },
+  {
+    "revision": "d31e4cf38885cec37118",
+    "url": "/haikus-ahoy/static/js/main.8e29ff68.chunk.js"
+  },
+  {
+    "revision": "f3b7d9ab14e3f29fdd16",
+    "url": "/haikus-ahoy/static/js/runtime~main.af42578f.js"
+  },
+  {
+    "revision": "177550eed09c82b9ac38f5746abbddc6",
+    "url": "/haikus-ahoy/static/media/modal.177550ee.svg"
+  }
+]);
